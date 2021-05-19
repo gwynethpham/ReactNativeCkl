a18f20a7189517fe9f35e9890838703fd5785c01
tan.phamminh <tan.phamminh@hdwebsoft.co>
2021-05-19 10:33:20 +0700

    update

--- script.sh
+++ script.sh
@@ -2,3 +2,14 @@ rm -rf node_modules
 echo "Remove node_modules"
 yarn
 echo "install success!"
+
+git pull orgin
+
+git checkout -b develop
+
+yarn
+echo "Checkout develop"
+
+git merge origin circleci-project-setup
+
+echo "MERGE SUCCESS!!"
