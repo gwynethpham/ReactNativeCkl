973526d89e41bbcb96ffe00dc4d8cb0d06e9a18b
tan.phamminh <tan.phamminh@hdwebsoft.co>
2021-05-19 11:52:27 +0700

    update

--- script.sh
+++ script.sh
@@ -1,19 +1,19 @@
-# echo "MERGE SUCCESS!!"
-
-git pull orgin
-
-# git merge origin master
-
-git merge origin/master
-
-git push --set-upstream origin develop
-
 echo "MERGE SUCCESS!!"
 
-git checkout master 
+# git pull orgin
 
-git merge origin/develop
+# # git merge origin master
 
-git push origin
+# git merge origin/master
+
+# git push --set-upstream origin develop
+
+# echo "MERGE SUCCESS!!"
+
+# git checkout master 
+
+# git merge origin/develop
+
+# git push origin
 
 # git push --set-upstream origin master
